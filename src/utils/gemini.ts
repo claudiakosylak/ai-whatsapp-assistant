4778536ba78c7332d848eb16a8fd3c04b5ea4ba9
Claudia Kosylak <claudiakosylak@gmail.com>
2025-03-13 19:34:31 +0100

    test

--- src/utils/gemini.ts
+++ src/utils/gemini.ts
@@ -15,7 +15,7 @@ export const processGeminiResponse = async (
   addLog('Processing Gemini response.');
   // context history has to start with a user message for gemini
   while (messageList[0].role === 'model') {
-    messageList.shift()
+    messageList.shift();
   }
   const client = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
   let systemInstruction;
@@ -31,27 +31,34 @@ export const processGeminiResponse = async (
   try {
     const chat = client.chats.create({
       model: GEMINI_MODEL,
-      config: { systemInstruction: systemInstruction, responseModalities: ["Text", "Image"] },
+      config: {
+        systemInstruction: systemInstruction,
+        responseModalities: ['Text', 'Image'],
+      },
       history: messageList,
     });
     response = await chat.sendMessage({ message: lastMessage.parts });
     // addLog(`Here is the gemini response: ${JSON.stringify(response)}`)
     if (response && response.candidates) {
-        response.candidates[0].content?.parts?.forEach(async (part) => {
-            if (part.inlineData && part.inlineData.data && part.inlineData.mimeType) {
-                // addLog(`Response part: ${JSON.stringify(part.fileData.fileUri)}`)
-                 const base64Data = part.inlineData.data.replace(
-                        /^data:image\/\w+;base64,/,
-                        '',
-                      );
-                media = {
-                    data: base64Data,
-                    mimetype: part.inlineData.mimeType,
-                    filename: null,
-                    filesize: null,
-                }
-            }
-        })
+      response.candidates[0].content?.parts?.forEach(async (part) => {
+        if (
+          part.inlineData &&
+          part.inlineData.data &&
+          part.inlineData.mimeType
+        ) {
+          // addLog(`Response part: ${JSON.stringify(part.fileData.fileUri)}`)
+          const base64Data = part.inlineData.data.replace(
+            /^data:image\/\w+;base64,/,
+            '',
+          );
+          media = new MessageMedia(
+            part.inlineData.mimeType,
+            base64Data,
+            null,
+            null,
+          );
+        }
+      });
     }
   } catch (error) {
     addLog(`Error fetching gemini response: ${error}`);
@@ -61,30 +68,37 @@ export const processGeminiResponse = async (
       rawText: 'Error',
     };
   }
-  addLog(`This is the media before sending back: ${media}`)
+  addLog(`This is the media before sending back: ${media}`);
   return {
     from,
-    messageContent: response.text ? response.text : media ? '' : 'There was a problem with your request.',
+    messageContent: response.text
+      ? response.text
+      : media
+      ? ''
+      : 'There was a problem with your request.',
     messageMedia: media,
     rawText: response.text || 'Error.',
   };
 };
 
-export const uploadImageToGemini = async (base64String: string, mimeType: string) => {
-    const client = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
-    try {
-        const imageBuffer = Buffer.from(base64String, 'base64');
-        const fileBlob = new Blob([imageBuffer], { type: mimeType });
-        let response = await client.files.upload({file: fileBlob})
-        const fileName = response.name;
-        addLog(`Image upload to gemini started.`)
-        while (response.state !== FileState.ACTIVE) {
-            response = await client.files.get({name: fileName as string})
-        }
-        addLog(`Image upload to gemini complete.`)
-        return response.uri;
-    } catch (error) {
-        addLog(`Error uploading image to Gemini`)
-        throw(error)
+export const uploadImageToGemini = async (
+  base64String: string,
+  mimeType: string,
+) => {
+  const client = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+  try {
+    const imageBuffer = Buffer.from(base64String, 'base64');
+    const fileBlob = new Blob([imageBuffer], { type: mimeType });
+    let response = await client.files.upload({ file: fileBlob });
+    const fileName = response.name;
+    addLog(`Image upload to gemini started.`);
+    while (response.state !== FileState.ACTIVE) {
+      response = await client.files.get({ name: fileName as string });
     }
-}
+    addLog(`Image upload to gemini complete.`);
+    return response.uri;
+  } catch (error) {
+    addLog(`Error uploading image to Gemini`);
+    throw error;
+  }
+};
