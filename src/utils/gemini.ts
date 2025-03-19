247a5a3a82c32c4fe4ef7011acd07228a54df544
Claudia Kosylak <claudiakosylak@gmail.com>
2025-03-19 23:07:21 +0100

    more logs

--- src/utils/gemini.ts
+++ src/utils/gemini.ts
@@ -61,6 +61,7 @@ export const processGeminiResponse = async (
         return;
       }
       if (imageUri) {
+        addLog(`There is a uri for the image/document`)
         let geminiMediaPart = {
           fileData: {
             fileUri: imageUri,
