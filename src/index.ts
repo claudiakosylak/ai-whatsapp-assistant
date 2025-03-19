6bf19b1ea5735674d02c039f75bd40349f4340f7
Claudia Kosylak <claudiakosylak@gmail.com>
2025-03-19 19:47:53 +0100

    message forwarding debug

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,50 @@
-import { Message, Client, LocalAuth } from "whatsapp-web.js";
-import qrcode from "qrcode-terminal"
-import { handleIncomingMessage } from "./utils/whatsapp";
-import { startControlPanel } from "./controlPanel";
-import { addLog, setWhatsAppConnected } from "./utils/controlPanel";
-import { getIsAudio, isMessageReceivedAfterInit } from "./utils/messages";
+import { Message, Client, LocalAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+import { handleIncomingMessage } from './utils/whatsapp';
+import { startControlPanel } from './controlPanel';
+import { addLog, setWhatsAppConnected } from './utils/controlPanel';
+import { isMessageReceivedAfterInit } from './utils/messages';
 
-const initTime = new Date()
+const initTime = new Date();
 
 const client = new Client({
-    authStrategy: new LocalAuth()
-  });
+  authStrategy: new LocalAuth(),
+});
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-  });
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
 
-  client.on('ready', () => {
-    addLog('WhatsApp client is ready');
-    setWhatsAppConnected(true);
-  });
-
-  client.on('disconnected', () => {
-    addLog('WhatsApp client disconnected');
-    setWhatsAppConnected(false);
-  });
-
-  client.on('message', async (message: Message) => {
-    // check that the new message was received after bot initiated
-    if (!isMessageReceivedAfterInit(initTime, message)) return
-    handleIncomingMessage(client, message)
-  });
-
-  try {
-    startControlPanel();
-    addLog('Starting WhatsApp client');
-    client.initialize();
-  }catch (e: any){
-    const errorMsg = `ERROR: ${e.message}`;
-    console.error(errorMsg);
-    addLog(errorMsg);
+client.on('ready', () => {
+  addLog('WhatsApp client is ready');
+  setWhatsAppConnected(true);
+  if (client.pupPage) {
+    client.pupPage.on('pageerror', function (err) {
+      console.log('Page error: ' + err.toString());
+    });
+    client.pupPage.on('error', function (err) {
+      console.log('Page error: ' + err.toString());
+    });
   }
+});
+
+client.on('disconnected', () => {
+  addLog('WhatsApp client disconnected');
+  setWhatsAppConnected(false);
+});
+
+client.on('message', async (message: Message) => {
+  // check that the new message was received after bot initiated
+  if (!isMessageReceivedAfterInit(initTime, message)) return;
+  handleIncomingMessage(client, message);
+});
+
+try {
+  startControlPanel();
+  addLog('Starting WhatsApp client');
+  client.initialize();
+} catch (e: any) {
+  const errorMsg = `ERROR: ${e.message}`;
+  console.error(errorMsg);
+  addLog(errorMsg);
+}
